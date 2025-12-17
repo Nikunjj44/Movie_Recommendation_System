a47eb065032a365aab4e61b835819af4e79e36cc
Nikunj Madan <nikunjmadan@Nikunjs-MacBook-Air.local>
2025-12-17 10:18:19 +0530

    streamlit app setup

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import os
+
+st.set_page_config(page_title = "Movie Recommendation")
+
+def main():
+    st.title("Movie Recommendations")
+
+
+if __name__ == "__main__":
+    main()
